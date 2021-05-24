55d74e7e2d3137d51c729f32a78b454960df87c4
Akihiro NAKAMURA <28667444+nakamurakko@users.noreply.github.com>
2021-05-24 11:18:03 +0900

    WPF の RelativeSource 確認用に、 WpfRelativeSourceSample 追加。

---
+++ WpfRelativeSourceSample/WpfRelativeSourceSample/DataTypes/Item.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfRelativeSourceSample.DataTypes
+{
+    public class Item: BindableBase
+    {
+        private string _id;
+        public string Id
+        {
+            get => _id;
+            set => SetProperty(ref _id, value);
+        }
+
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+    }
+}
---
+++ WpfRelativeSourceSample/WpfRelativeSourceSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.ObjectModel;
+using WpfRelativeSourceSample.DataTypes;
+
+namespace WpfRelativeSourceSample.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+
+        public ObservableCollection<Item> Items { get; private set; }
+
+        /// <summary>
+        /// Item 削除コマンド。
+        /// </summary>
+        public DelegateCommand<Item> DeleteItemCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            Items = new ObservableCollection<Item>()
+            {
+                new Item() { Id = Guid.NewGuid().ToString(), Name = "Item1" },
+                new Item() { Id = Guid.NewGuid().ToString(), Name = "Item2" },
+                new Item() { Id = Guid.NewGuid().ToString(), Name = "Item3" },
+                new Item() { Id = Guid.NewGuid().ToString(), Name = "Item4" },
+                new Item() { Id = Guid.NewGuid().ToString(), Name = "Item5" },
+            };
+
+            DeleteItemCommand = new DelegateCommand<Item>((item) => DeleteItemCommandExecute(item));
+        }
+
+        /// <summary>
+        /// Item 削除処理。
+        /// </summary>
+        /// <param name="item">対象の Item。</param>
+        private void DeleteItemCommandExecute(Item item)
+        {
+            Items.Remove(item);
+        }
+    }
+}
